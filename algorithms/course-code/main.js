9b6927f38f19d657870fcec1bf5e8a86cca7edf0
ya77ia <ylebbah@gmail.com>
2024-10-14 05:24:06 +0100

    binary search

--- algorithms/course-code/main.js
+++ algorithms/course-code/main.js
@@ -71,6 +71,7 @@ function printArray(arr) {
 // ---------------------------------------
 // ---------------------------------------
 // ---------------------------------------
+// https://www.geeksforgeeks.org/merge-sort/
 
 function merge(arr, left, mid, right) {
     const n1 = mid - left + 1;
@@ -144,4 +145,72 @@ function PuissDyn(x, n) {
         res = x*PuissDyn(x*x, (n - 1)/2);
     console.log(`PuissDyn(${x}, ${n}) = ${res}`);
     return (res);    
+}
+
+// ---------------------------------------
+// ---------------------------------------
+// ---------------------------------------
+// ---------------------------------------
+// ---------------------------------------
+// https://www.geeksforgeeks.org/binary-search/
+
+function binarySearchIterative(arr, x)
+{
+    let low = 0;
+    let high = arr.length - 1;
+    let mid;
+    while (high >= low) {
+        mid = low + Math.floor((high - low) / 2);
+
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            high = mid - 1;
+
+        // Else the element can only be present
+        // in right subarray
+        else
+            low = mid + 1;
+    }
+
+    // We reach here when element is not
+    // present in array
+    return -1;
+}
+
+// ---------------------------------------
+// ---------------------------------------
+// ---------------------------------------
+// ---------------------------------------
+// ---------------------------------------
+// https://www.geeksforgeeks.org/binary-search/
+
+function binarySearchRecursive(arr, low, high, x)
+{
+    if (high >= low) {
+        let mid = low + Math.floor((high - low) / 2);
+
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            return binarySearchRecursive(arr, low, mid - 1, x);
+
+        // Else the element can only be present
+        // in right subarray
+        return binarySearchRecursive(arr, mid + 1, high, x);
+    }
+
+    // We reach here when element is not
+    // present in array
+    return -1;
 }
